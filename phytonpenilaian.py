c828f71075d5cde2d557d51496b81bd00974fe7b
hafiidaminudin <hafiidaminudin7@gmail.com>
2024-12-19 11:44:41 +0700

    Add files via upload

---
+++ phytonpenilaian.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 19 11:25:58 2024
+
+@author: ASUS
+"""
+  
+n=int (input("masukkan nilai n :"))
+total=0
+i=1
+for i in range (1, n+1):
+  total += 1
+  
+r= float (input("Masukan jari-jari (r) :"))
+luas_lingkaran= 3.14*r*r
+print(luas_lingkaran)
+
+
+
+for in range(1,6)
+    i<6?
+    print(i)
+    i+1
+    
+    
+for in range(1,11,2):
+    i <11?
+    print(i)
+    i+2
